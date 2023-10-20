4b63ebb913c306d588fc7fdcb4131c967c6146c9
Mark Aurel <markaureldev@gmail.com>
2023-10-20 05:08:52 +0000

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -9,31 +9,28 @@ export default async function middleware(req: NextRequest) {
   const url = req.nextUrl;
 
   // Get hostname (e.g. vercel.com, test.vercel.app, etc.)
-  let hostname = req.headers.get("host");
+  const hostname = req.headers.get("host");
 
-  // If in a production environment on Vercel, adjust the host value
-  if (process.env.VERCEL_ENV === "production") {
-    const customDomain = hostname.includes(".bauzito.shop");
-    hostname = customDomain
-      ? hostname.replace(`.bauzito.shop`, "")
-      : hostname.replace(`.vercel.app`, "");
-  }
+  // If localhost, assign the host value manually
+  // If prod, get the custom domain/subdomain value by removing the root URL
+  // (in the case of "subdomain-3.localhost:3000", "localhost:3000" is the root URL)
+  // process.env.NODE_ENV === "production" indicates that the app is deployed to a production environment
+  // process.env.VERCEL === "1" indicates that the app is deployed on Vercel
+  const currentHost =
+    process.env.NODE_ENV === "production" && process.env.VERCEL === "1"
+      ? hostname.replace(`.bauzito.app`, "")
+      : hostname.replace(`.csb.app`, "");
 
-  try {
-    const data = await getHostnameDataOrDefault(currentHost);
-    console.log(data);
+  const data = await getHostnameDataOrDefault(currentHost);
 
-    // Prevent security issues – users should not be able to canonically access
-    // the pages/sites folder and its respective contents.
-    if (url.pathname.startsWith(`/_sites`)) {
-      url.pathname = `/404`;
-    } else {
-      // console.log('URL 2', req.nextUrl.href)
-      // rewrite to the current subdomain under the pages/sites folder
-      url.pathname = `/_sites/${data.subdomain}${url.pathname}`;
-    }
-  } catch (error) {
-    console.error("Error while fetching data:", error);
+  // Prevent security issues – users should not be able to canonically access
+  // the pages/sites folder and its respective contents.
+  if (url.pathname.startsWith(`/_sites`)) {
+    url.pathname = `/404`;
+  } else {
+    // console.log('URL 2', req.nextUrl.href)
+    // rewrite to the current subdomain under the pages/sites folder
+    url.pathname = `/_sites/${data.subdomain}${url.pathname}`;
   }
 
   return NextResponse.rewrite(url);
